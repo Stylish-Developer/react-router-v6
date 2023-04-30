e63e7b381a52edc848c2e4b3597a1e91f52a0d31
Gowtham <gowthamsgobi@gmail.com>
2023-04-30 14:31:57 +0530

    404 page workouted with path=" * "

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/navbar";
 import OrderSummary from "./components/OrderSummary";
+import _404 from './components/404';
 
 // -- app component
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
         <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<_404 />} />
       </Routes>
     </>
   );
---
+++ src/components/404.js
@@ -0,0 +1,27 @@
+import _404Img from "../assets/img/404.jpg";
+
+// -- 404 page for this app
+const _404 = () => {
+  const img404 = {
+    height: "50%",
+    width: "50%",
+    ObjectFit: "fill",
+  };
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent:'center'
+        }}
+      >
+        <img src={_404Img} alt="404_img" style={img404} />
+      </div>
+    </>
+  );
+};
+
+export default _404;
